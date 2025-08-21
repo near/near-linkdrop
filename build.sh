ba5609176075cf068945de0e97111053b44d0d9a
Vlad Frolov <frolvlad@gmail.com>
2025-08-21 16:04:34 +0200

    refactor: Upgraded near-sdk-rs from 4.0.0 to 5.17.0 (#35)

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-docker run -it --rm -v "$(pwd):/mnt" --workdir /mnt sourcescan/cargo-near:0.10.0-rust-1.81.0 bash -c '
-  RUSTFLAGS="-C link-arg=-s" cargo build --target wasm32-unknown-unknown --release
-  cp target/wasm32-unknown-unknown/release/linkdrop.wasm ./res/
-'
+cargo near build reproducible-wasm
+
--- src/models.rs
+++ src/models.rs
@@ -1,39 +1,35 @@
 use near_sdk::json_types::Base64VecU8;
-use near_sdk::serde::{Serialize, Deserialize};
+use near_sdk::near;
 
 use crate::*;
 
 /// Information about a specific public key. Should be returned in the `get_key_information` view method.
 /// Part of the linkdrop NEP
-#[derive(Serialize)]
-#[serde(crate = "near_sdk::serde")]
+#[near(serializers=[json])]
 pub struct KeyInfo {
     /// yoctoNEAR$ amount that will be sent to the claiming account (either new or existing)
     /// when the key is successfully used.
-    pub balance: U128,
+    pub balance: NearToken,
 }
 
-
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "near_sdk::serde")]
 /// Information about any limited access keys that are being added to the account as part of `create_account_advanced`.
+#[near(serializers=[json])]
 pub struct LimitedAccessKey {
     /// The public key of the limited access key.
     pub public_key: PublicKey,
     /// The amount of yoctoNEAR$ that can be spent on Gas by this key.
-    pub allowance: U128,
+    pub allowance: NearToken,
     /// Which contract should this key be allowed to call.
     pub receiver_id: AccountId,
     /// Which methods should this key be allowed to call.
     pub method_names: String,
 }
-    
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "near_sdk::serde")]
+
 /// Options for `create_account_advanced`.
+#[near(serializers=[json])]
 pub struct CreateAccountOptions {
     pub full_access_keys: Option<Vec<PublicKey>>,
     pub limited_access_keys: Option<Vec<LimitedAccessKey>>,
     pub contract_bytes: Option<Vec<u8>>,
-    pub contract_bytes_base64: Option<Base64VecU8>
+    pub contract_bytes_base64: Option<Base64VecU8>,
 }
